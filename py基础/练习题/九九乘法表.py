327eaedbe443a778d9ddac97557ccddc12a0e974
liaoxingzhi <42024275+liaoxingzhi@users.noreply.github.com>
2022-06-08 00:00:00 +0800

    注册登录

--- py基础/练习题/九九乘法表.py
+++ py基础/练习题/九九乘法表.py
@@ -3,6 +3,8 @@
 # File : 九九乘法表
 
 for i in range(1, 10):
+    # 每次外循环开始打印空格1~9
     print()
     for j in range(1, i + 1):
+        # end=' ':每次内循环以end里面的内容连接
         print(j, '*', i, '=', j * i, end='  ')
